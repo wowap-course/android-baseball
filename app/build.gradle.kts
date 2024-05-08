df9e133715b9eb27ef1cf2e1169a1e79dbf1b3e7
vrexpert <vrexpert2302@gmail.com>
2024-05-08 23:09:52 +0900

    feat: 수업 자료 화면 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/baseball/MainActivity.kt
+++ app/src/main/java/com/example/baseball/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.baseball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.baseball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initCount()
+        initBtn()
+    }
+
+    private fun initCount() {
+        binding.tvCount.text = count.toString()
+    }
+
+    private fun initBtn() {
+        binding.btnPlus.setOnClickListener {
+            count++
+            binding.tvCount.text = count.toString()
+        }
+        binding.btnMinus.setOnClickListener {
+            count--
+            binding.tvCount.text = count.toString()
+        }
     }
 }
