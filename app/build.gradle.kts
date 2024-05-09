aa0a4057d82fbecfc221bdc91c16f2e5d7aba6a6
vrexpert <vrexpert2302@gmail.com>
2024-05-09 17:03:00 +0900

    feat: test 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.activity:activity-ktx:1.9.0")
     testImplementation("junit:junit:4.13.2")
+
+    //assertThat
+    testImplementation("org.assertj:assertj-core:3.25.3")
+
+    //assertThrows
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
