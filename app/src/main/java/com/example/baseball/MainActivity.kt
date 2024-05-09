0cb4fc117484546ee8623bd971653e9e4d36ecb2
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-09 12:23:18 +0900

    feat : Count 세는 변수 화면에 나타내기

--- app/src/main/java/com/example/baseball/MainActivity.kt
+++ app/src/main/java/com/example/baseball/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.baseball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.baseball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initCount()
     }
+
+    private fun initCount() {
+        binding.tvCount!!.text = count.toString()
+        }
 }
