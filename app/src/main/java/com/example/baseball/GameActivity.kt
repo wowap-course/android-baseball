a4e37b3fd58a7379a51a39316df385c1b1d9d66e
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-10 20:20:21 +0900

    feat : 뒤로가기 구현하기

--- app/src/main/java/com/example/baseball/GameActivity.kt
+++ app/src/main/java/com/example/baseball/GameActivity.kt
@@ -1,14 +1,20 @@
 package com.example.baseball
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.baseball.databinding.ActivityMainBinding
+import android.widget.ImageView
+import android.widget.TextView
 
-class GameActivity : AppCompatActivity() {
+class GameActivity: Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_main)
+        val imageView = findViewById<ImageView>(R.id.backBtn)
+        imageView.setOnClickListener {onImageClick()}
+    }
 
+    private fun onImageClick() {
+        finish()
     }
 }
