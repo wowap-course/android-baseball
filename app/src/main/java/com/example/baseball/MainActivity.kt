f2144fa9f07c4d39f5bc5e57173bc45d63bf686a
okdoittttt <sonokmoo@gmail.com>
2024-05-10 18:39:37 +0900

    feat: 메인 액티비티 레이아웃 연결, 버튼 터치시 값 상승, 하락 구현

--- app/src/main/java/com/example/baseball/MainActivity.kt
+++ app/src/main/java/com/example/baseball/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.baseball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.baseball.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initCount()
+        initBtn()
+    }
+
+    private fun initCount() {
+        binding.txCount.text = count.toString()
+    }
+
+    private fun initBtn() {
+        binding.btnPlus.setOnClickListener {
+            count++
+            binding.txCount.text = count.toString()
+        }
+        binding.btnMinus.setOnClickListener {
+            count--
+            binding.txCount.text = count.toString()
+        }
     }
 }
