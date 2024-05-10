63327950b301a12a07dfbe32d0e869b74303afc0
okdoittttt <sonokmoo@gmail.com>
2024-05-10 20:00:57 +0900

    feat: 횟수가 1부터 10까지만 설정되도록 구현

--- app/src/main/java/com/example/baseball/MainActivity.kt
+++ app/src/main/java/com/example/baseball/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var count = 0
+    private var count = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,12 +24,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun initBtn() {
         binding.btnPlus.setOnClickListener {
-            count++
-            binding.txCount.text = count.toString()
+            if (count < 10) {
+                count++
+                binding.txCount.text = count.toString()
+            }
         }
         binding.btnMinus.setOnClickListener {
-            count--
-            binding.txCount.text = count.toString()
+            if (count > 1) {
+                count--
+                binding.txCount.text = count.toString()
+            }
         }
     }
 }
