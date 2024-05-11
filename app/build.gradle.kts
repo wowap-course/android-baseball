165d738432a67ca14c9de9c56bfdbcf41fc92dc2
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-11 17:05:10 +0900

    chore : 안드로이드 스튜디오 기본설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,10 +67,15 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+//    testImplementation 'org.junit.jupiter:junit-jupiter:5.8.2'
+//    testImplementation 'org.assertj:assertj-core:3.23.1'
+    testImplementation("org.junit.jupiter", "junit-jupiter", "5.10.2")
+    testImplementation("org.assertj", "assertj-core", "3.25.3")
 }
