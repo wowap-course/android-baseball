9524f69dc5cd1c5f9691336f78992775a06d28b5
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-05-12 21:52:19 +0900

    feat(GameActivity): 뒤로가기 버튼을 누르면 MainActivity로 전환된다

--- app/src/main/java/com/example/baseball/GameActivity.kt
+++ app/src/main/java/com/example/baseball/GameActivity.kt
@@ -9,12 +9,9 @@ import androidx.core.view.WindowInsetsCompat
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_game)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        supportActionBar?.title = "Baseball"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
