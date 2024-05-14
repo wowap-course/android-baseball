5e283d02c266ec06bc3310741453159635ca8a87
vrexpert <vrexpert2302@gmail.com>
2024-05-14 17:37:54 +0900

    feat: 숫자야구 게임 화면 정의

---
+++ app/src/main/java/com/example/baseball/presentation/GameActivity.kt
@@ -0,0 +1,21 @@
+package com.example.baseball.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.baseball.databinding.ActivityGameBinding
+
+class GameActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityGameBinding
+    private var life: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+
+        life = intent.getIntExtra("life", 1)
+        binding.tvLife.text = life.toString()
+
+        setContentView(binding.root)
+    }
+}
