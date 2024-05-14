2d3ff9a58d49b04ee6c55aa07469a234bc77d82d
vrexpert <vrexpert2302@gmail.com>
2024-05-14 17:39:58 +0900

    feat(MainActivity) : 메인 화면에서 목숨의 개수를 결정해 게임 화면을 시작할 수 있다

--- app/src/main/java/com/example/baseball/presentation/MainActivity.kt
+++ app/src/main/java/com/example/baseball/presentation/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.baseball.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import com.example.baseball.databinding.ActivityMainBinding
+import com.example.baseball.domain.model.Life
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var count = 0
+    private var life = Life(1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,21 +20,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initCount()
-        initBtn()
+        initPlusBtn()
+        initMinusBtn()
+        initResetBtn()
     }
 
     private fun initCount() {
-        binding.tvCount.text = count.toString()
+        binding.tvLife.text = life.value.toString()
     }
 
-    private fun initBtn() {
-        binding.btnPlus.setOnClickListener {
-            count++
-            binding.tvCount.text = count.toString()
-        }
+    private fun initMinusBtn() {
         binding.btnMinus.setOnClickListener {
-            count--
-            binding.tvCount.text = count.toString()
+            life--
+            binding.tvLife.text = life.value.toString()
+        }
+    }
+
+    private fun initPlusBtn() {
+        binding.btnPlus.setOnClickListener {
+            life++
+            binding.tvLife.text = life.value.toString()
+        }
+    }
+
+    private fun initResetBtn() {
+        binding.btnStartGame.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            intent.putExtra("life", life.value)
+            startActivity(intent)
         }
     }
 }
