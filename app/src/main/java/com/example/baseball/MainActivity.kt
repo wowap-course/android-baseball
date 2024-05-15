0e8bd47c2921b4dad217f7bd2718096797b2e149
okdoittttt <sonokmoo@gmail.com>
2024-05-15 21:10:27 +0900

    feat: MainActivity를 View로 사용하기 위한 코드 구현 - showLife 함수를 오버라이딩하여 XML에 정의된 textView에 값을 부여 - 감소 버튼에 리스너 구현 - 증가 버튼에 리스너 구현 - 다음 액티비티에게 값을 넘겨줄 버튼 함수 구현(binding 사용하여 현재 출력되고 있는 값을 변수로 받아와 전달하는 방식으로 구현. 해당 방식이 효율적인지 추후 고민 예정)

--- app/src/main/java/com/example/baseball/MainActivity.kt
+++ app/src/main/java/com/example/baseball/MainActivity.kt
@@ -4,46 +4,51 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.baseball.databinding.ActivityMainBinding
+import com.example.baseball.domain.Life
+import com.example.baseball.domain.Life.Companion.MIN_LIFE
+import com.example.baseball.presentation.MainContract
+import com.example.baseball.presentation.MainPresenter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainContract.View {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var count = 1
+    private lateinit var presenter: MainContract.Presenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initCount()
-        initBtn()
+        initPresenter()
+        initBtnPlus()
+        initBtnMinus()
         initStartButton()
     }
 
-    private fun initCount() {
-        binding.txCount.text = count.toString()
+    private fun initPresenter() {
+        presenter = MainPresenter(this)
     }
 
-    private fun initBtn() {
-        binding.btnPlus.setOnClickListener {
-            if (count < 10) {
-                count++
-                binding.txCount.text = count.toString()
-            }
-        }
-        binding.btnMinus.setOnClickListener {
-            if (count > 1) {
-                count--
-                binding.txCount.text = count.toString()
-            }
-        }
+    override fun showLife(life: Int) {
+        binding.txCount.text = life.toString()
+    }
+
+    private fun initBtnPlus() {
+        binding.btnPlus.setOnClickListener { presenter.increaseLife() }
+    }
+
+    private fun initBtnMinus() {
+        binding.btnMinus.setOnClickListener { presenter.decreaseLife() }
     }
 
     private fun initStartButton() {
         binding.btnStart.setOnClickListener {
+            val txCountValue = binding.txCount.text.toString().toIntOrNull() ?: 0
             val intent = Intent(this, GetCountActivity::class.java)
-            intent.putExtra("count", count)
+            intent.putExtra("count", txCountValue)
             startActivity(intent)
         }
     }
+
 }
