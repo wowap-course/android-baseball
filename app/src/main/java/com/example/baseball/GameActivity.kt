7cc59524f96ec82c24f7d759f73b8b4c38be679a
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-05-17 17:23:33 +0900

    feat(GameActivity): 결과 화면 생성

--- app/src/main/java/com/example/baseball/GameActivity.kt
+++ app/src/main/java/com/example/baseball/GameActivity.kt
@@ -1,6 +1,9 @@
 package com.example.baseball
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.baseball.databinding.ActivityGameBinding
 
@@ -14,15 +17,41 @@ class GameActivity : AppCompatActivity() {
 
         initTitleBar()
         initTextView()
+        initButton()
     }
 
     private fun initTitleBar(){
-        supportActionBar?.title = "Baseball"
+        supportActionBar?.title = getString(R.string.app_name)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     private fun initTextView(){
         val lifeCount = intent.getIntExtra("lifeCount", 0)
         binding.tvRemainingLives.text = String.format(getString(R.string.remaining_life), lifeCount)
+    }
+
+    private fun initButton() {
+        binding.btnTry.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        var resultDialog = AlertDialog.Builder(this)
+        resultDialog.setTitle(getString(R.string.success))
+        resultDialog.setMessage(String.format(getString(R.string.answer), ))
+
+        val listener = DialogInterface.OnClickListener { dialog, which ->
+            when(which) {
+                DialogInterface.BUTTON_POSITIVE ->
+                    Log.d("dialog", "positive")
+
+                DialogInterface.BUTTON_NEGATIVE ->
+                    Log.d("dialog", "negative")
+            }
+        }
+        resultDialog.setPositiveButton(getString(R.string.restart), listener)
+        resultDialog.setNegativeButton(getString(R.string.exit), listener)
+        resultDialog.show()
     }
 }
