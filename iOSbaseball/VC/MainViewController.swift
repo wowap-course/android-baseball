35ab7b5447382e5acb65744b85e5d2c15d9b9f22
김민석|Minseok Kim <alstjr7437@naver.com>
2024-05-18 20:03:04 +0900

    chore: 폴더 기본 구조 세팅

---
+++ iOSbaseball/VC/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSbaseball
+//
+//  Created by KimMinSeok on 5/18/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSbaseball/domain/LifeCount.swift
@@ -0,0 +1,12 @@
+//
+//  LifeCount.swift
+//  iOSbaseball
+//
+//  Created by KimMinSeok on 5/18/24.
+//
+
+import Foundation
+
+class LifeCount{
+    
+}
