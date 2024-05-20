8ace13b991934678a383703eae9b20dcaf5c1230
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-05-20 13:21:03 +0900

    feat(LifeCount): value class로 변경

--- app/src/main/java/com/example/baseball/domain/LifeCount.kt
+++ app/src/main/java/com/example/baseball/domain/LifeCount.kt
@@ -1,22 +1,17 @@
 package com.example.baseball.domain
 
-data class LifeCount(private var _count : Int = 1) {
+@JvmInline
+value class LifeCount(val count : Int = 1) {
     init {
-        require(_count in MIN_LIFE_COUNT..MAX_LIFE_COUNT)
-    }
-    val count : Int
-        get() = _count
-
-    fun increase() : Boolean {
-        if(_count >= MAX_LIFE_COUNT) return false
-        _count++
-        return true
+        require(count in MIN_LIFE_COUNT..MAX_LIFE_COUNT)
     }
 
-    fun decrease() : Boolean {
-        if(_count <= MIN_LIFE_COUNT) return false
-        _count--
-        return true
+    operator fun inc() : LifeCount {
+        return if(count > MAX_LIFE_COUNT) LifeCount(MAX_LIFE_COUNT) else LifeCount(count + 1)
+    }
+
+    operator fun dec() : LifeCount {
+        return if(count < MIN_LIFE_COUNT) LifeCount(MIN_LIFE_COUNT) else LifeCount(count - 1)
     }
     companion object {
         private const val MIN_LIFE_COUNT = 1
