140c5eb788f788b6bcee268ce1bb3dfe542b4a1a
김민석|Minseok Kim <alstjr7437@naver.com>
2024-05-20 20:54:16 +0900

    feat: 목숨 개수(domain)를 TDD를 사용하여 구현

--- iOSbaseball/domain/LifeCount.swift
+++ iOSbaseball/domain/LifeCount.swift
@@ -4,9 +4,35 @@
 //
 //  Created by KimMinSeok on 5/18/24.
 //
+enum LifeCountError: Error {
+    case IllegalArgumentException
+}
 
 import Foundation
 
 class LifeCount{
+    private(set) var lifes : Int
+    init(lifes: Int = 1) throws {
+        self.lifes = lifes
+        
+        guard (MIN_LIFE_COUNT...MAX_LIFE_COUNT).contains(self.lifes) else {
+             throw LifeCountError.IllegalArgumentException
+        }
+    }
     
+    func increase() {
+        if lifes < MAX_LIFE_COUNT {
+            lifes += 1
+        }
+        
+    }
+    
+    func decrease() {
+        if lifes > MIN_LIFE_COUNT {
+            lifes -= 1
+        }
+    }
+    
+    private let MIN_LIFE_COUNT = 1
+    private let MAX_LIFE_COUNT = 9
 }
---
+++ iOSbaseballTests/LifeCountTests.swift
@@ -0,0 +1,83 @@
+//
+//  iOSbaseballTests.swift
+//  iOSbaseballTests
+//
+//  Created by KimMinSeok on 5/18/24.
+//
+
+import XCTest
+@testable import iOSbaseball
+
+final class iOSbaseballTests: XCTestCase {
+    func test_처음_목숨의_개수는_1개이다() throws {
+        let lifeCount = try LifeCount().lifes
+        
+        XCTAssertEqual(lifeCount, 1)
+    }
+    
+    func test_목숨은_1에서_10_사이의_숫자이다() throws {
+        // Given
+        let lifes = 11
+        
+        // Then
+        XCTAssertThrowsError(try LifeCount(lifes: lifes))
+    }
+    
+    func test_목숨은_1에서_10_사이의_숫자이다2() throws {
+        // Given
+        let lifes = 0
+        
+        // Then
+        XCTAssertThrowsError(try LifeCount(lifes: lifes))
+    }
+    
+    func test_목숨을_추가할_수_있다() throws {
+        // Given
+        let lifeCount = try LifeCount()
+        
+        // When
+        lifeCount.increase()
+        
+        // Then
+        XCTAssertEqual(lifeCount.lifes, 2)
+    }
+    
+    func test_목숨을_뺄_수_있다() throws {
+        // Given
+        let lifeCount = try LifeCount(lifes: 2)
+        
+        // When
+        lifeCount.decrease()
+        
+        // Then
+        XCTAssertEqual(lifeCount.lifes, 1)
+    }
+    
+    func test_9개의_목숨에서_추가는_되지_않는다() throws {
+        // Given
+        let lifeCount = try LifeCount(lifes: 9)
+        
+        // When
+        lifeCount.increase()
+        
+        // Then
+        XCTAssertEqual(lifeCount.lifes, 9)
+        XCTAssertNotEqual(lifeCount.lifes, 10)
+        
+    }
+
+    func test_1개의_목숨에서_빼지지_않는다() throws {
+        // Given
+        let lifeCount = try LifeCount(lifes: 1)
+        
+        // When
+        lifeCount.decrease()
+        
+        // Then
+        XCTAssertEqual(lifeCount.lifes, 1)
+        XCTAssertNotEqual(lifeCount.lifes, 0)
+        
+    }
+
+    
+}
