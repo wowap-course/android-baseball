913be05db976fe010a098cca156213e2192534fd
김민석|Minseok Kim <alstjr7437@naver.com>
2024-05-20 21:49:51 +0900

    faet: 목숨 개수의 텍스트 확인 및 증가 감소 구현

--- iOSbaseball/VC/StartViewController.swift
+++ iOSbaseball/VC/StartViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
+enum RestartError: Error {
+    case IllegalArgumentException
+}
+
 class StartViewController: UIViewController {
 
+    @IBOutlet weak var lifeLabel: UILabel!
+    
+    var lifeCount: LifeCount! // 옵셔널 제거
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        do {
+            try initLifeCount() // lifeCount 초기화 추가
+            try initLifeLabel()
+        } catch {
+            print("에러 발생: \(error)")
+        }
+        
+    }
+    
+    private func initLifeCount() throws {
+        lifeCount = try LifeCount()
     }
 
-
+    private func initLifeLabel() throws {
+        lifeLabel.text = String(lifeCount.lifes)
+    }
+    
+    @IBAction func btnMinus(_ sender: Any) {
+        lifeCount.decrease()
+        lifeLabel.text = String(lifeCount.lifes)
+    }
+    
+    @IBAction func btnPlus(_ sender: Any) {
+        lifeCount.increase()
+        lifeLabel.text = String(lifeCount.lifes)
+    }
 }
 
