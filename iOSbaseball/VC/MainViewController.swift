0f0ada228b8cee45b8fa1852fe77c7e23bdcd078
김민석|Minseok Kim <alstjr7437@naver.com>
2024-05-21 18:38:41 +0900

    feat: 도전시 목숨이 줄어들고 끝나는 로직 구현

--- iOSbaseball/VC/MainViewController.swift
+++ iOSbaseball/VC/MainViewController.swift
@@ -12,11 +12,24 @@ class MainViewController: UIViewController {
     @IBOutlet weak var lifeLabel: UILabel!
     
     var lifeCount : LifeCount?
+    @IBOutlet weak var inputBall: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         initBtn()
         initLifeLabel()
+        
+    }
+    @IBAction func challengeBtn(_ sender: Any) {
+        
+        inputBall.text = ""
+        
+        if lifeCount!.decrease() {
+            initLifeLabel()
+        } else {
+            lifeLabel.text = "남은 목숨 : \(0)"
+            makeAlertDialog(title: "실패", message: "정답\(123)")
+        }
         
     }
     
@@ -34,6 +47,34 @@ class MainViewController: UIViewController {
         lifeLabel.text = "남은 목숨 : \(lifeCount!.lifes)"
     }
 
+    // Alert Dialog 생성
+    func makeAlertDialog(title: String, message: String, _ isAlert : Bool = true) {
+
+        let alert = isAlert ? UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        : UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        
+        let alertDeleteBtn = UIAlertAction(title: "재시작", style: .default) { (action) in
+            print("재시작 버튼 클릭")
+            self.dismiss(animated: true, completion: nil)
+        }
+        let alertSuccessBtn = UIAlertAction(title: "나가기", style: .destructive) { (action) in
+            print("나가기 버튼 클릭")
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        if(isAlert) {
+            alert.addAction(alertDeleteBtn)
+            alert.addAction(alertSuccessBtn)
+        }
+        else {
+            alert.addAction(alertSuccessBtn)
+            alert.addAction(alertDeleteBtn)
+        }
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
