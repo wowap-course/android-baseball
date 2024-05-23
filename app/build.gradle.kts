f0b88fec6038c33637b6c18f98c2b662cdcc2a9f
Xinu <66197586+Jinu219@users.noreply.github.com>
2024-05-23 12:23:36 +0900

    refactor : 도메인 모델 분리 ( 멀티 모듈 )

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,39 +40,27 @@ android {
     }
     buildFeatures {
         viewBinding = true
-        compose = true
     }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
 }
 
 dependencies {
-
+    implementation(project(":domain"))
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity:1.9.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-graphics-android:1.6.7")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation("org.junit.jupiter", "junit-jupiter", "5.10.2")
     testImplementation("org.assertj", "assertj-core", "3.25.3")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
---
+++ domain/src/test/kotlin/domain/LifeTest.kt
@@ -0,0 +1,66 @@
+package com.example.baseball.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertThrows
+import org.junit.Test
+
+class LifeTest {
+    @Test
+    fun `life는 1 밑으로 내려가면 안된다`() {
+        assertThrows<IllegalArgumentException> { Life(0) }
+    }
+
+    @Test
+    fun `life는 10 위로 올라가면 안된다`() {
+        assertThrows<IllegalArgumentException> { Life(11) }
+    }
+
+    @Test
+    fun `life가 5 일때 증가시키면 6이 된다`() {
+        // given
+        val life = Life(5)
+
+        // when
+        val actual = life.inc()
+
+        // then
+        assertThat(actual).isEqualTo(Life(6))
+    }
+
+    @Test
+    fun `life가 3 일때 감소시키면 2이 된다`() {
+        // given
+        val life = Life(3)
+
+        // when
+        val actual = life.dec()
+
+        // then
+        assertThat(actual).isEqualTo(Life(2))
+    }
+
+    @Test
+    fun `life가 10 일때 증가시키면 10이 된다`() {
+        // given
+        val life = Life(10)
+
+        // when
+        val actual = life.inc()
+
+        // then
+        assertThat(actual).isEqualTo(Life(10))
+    }
+
+    @Test
+    fun `life가 1 일때 감소시키면 1이 된다`() {
+        // given
+        val life = Life(1)
+
+        // when
+        val actual = life.dec()
+
+        // then
+        assertThat(actual).isEqualTo(Life(1))
+    }
+
+}
