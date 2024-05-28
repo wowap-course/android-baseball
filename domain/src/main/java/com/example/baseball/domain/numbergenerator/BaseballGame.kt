36be7f11439711042dcd735052e1c35789422ec4
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-05-29 00:17:03 +0900

    feat(BaseballGame): 게임의 정답을 만들고 Judgement를 통해 게임을 진행한다
    GamePresenter에서 로직을 줄이기 위해 전반적인 게임의 흐름을 제어하는 도메인을 생성하였다. 이로써 recyclerview의 dataset도 분리할 수 있다.

---
+++ domain/src/main/java/com/example/baseball/domain/numbergenerator/BaseballGame.kt
@@ -0,0 +1,28 @@
+package com.example.baseball.domain.numbergenerator
+
+import com.example.baseball.domain.BaseballNumber
+import com.example.baseball.domain.BaseballNumbers
+import com.example.baseball.domain.BaseballScore
+import com.example.baseball.domain.Judgement
+
+class BaseballGame() {
+    private var tryCount = INIT_TRY
+    private val judgement = Judgement()
+    private val opponentNumber: List<BaseballNumber> =
+        BaseballNumbers(RandomNumberGenerator().generateNumber()).baseballNumbers
+    private val _baseballScores = ArrayList<BaseballScore>()
+    val baseballScores get() = _baseballScores.toList()
+    fun playOnrRound(answer: String): BaseballScore {
+        val baseballNumberAnswer = BaseballNumbers(
+            answer.toList().map { it.toString().toInt() }).baseballNumbers // 올바른 값만 입력된다고 가정
+        val scoreOfInning = judgement.judgeNumber(tryCount++, opponentNumber, baseballNumberAnswer)
+
+        _baseballScores.add(0, scoreOfInning)
+
+        return scoreOfInning
+    }
+
+    companion object {
+        private const val INIT_TRY = 0
+    }
+}
