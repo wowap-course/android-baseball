20e3474f816c9ad1d0ab4ea98f676eca7719d9c3
gyunho <124599614+g0rnn@users.noreply.github.com>
2024-05-30 11:35:57 +0900

    refactor(RecyclerViewAdapter): viewHolder에서 binding을 사용하여 view에 접근

--- app/src/main/java/com/example/baseball/presentation/game/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/baseball/presentation/game/RecyclerViewAdapter.kt
@@ -1,31 +1,30 @@
 package com.example.baseball.presentation.game
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.baseball.R
+import com.example.baseball.databinding.ResultItemsBinding
 
 class RecyclerViewAdapter() : RecyclerView.Adapter<RecyclerViewAdapter.GameResultViewHolder>() {
-    private val dataSet : MutableList<Array<String>> = ArrayList<Array<String>>()
-    class GameResultViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-        private val tvTryCount : TextView = view.findViewById(R.id.tvTryCount)
-        private val tvBall : TextView = view.findViewById(R.id.tvBall)
-        private val tvStrike : TextView = view.findViewById(R.id.tvStrike)
-        private val tvAnswer : TextView = view.findViewById(R.id.tvAnswer)
+    private val dataSet : MutableList<Array<Int>> = ArrayList()
 
-        fun bind(gameScore : Array<String>) {
-            tvTryCount.text = gameScore[0]
-            tvBall.text = gameScore[1]
-            tvStrike.text = gameScore[2]
-            tvAnswer.text = gameScore[3]
+    class GameResultViewHolder(private val binding : ResultItemsBinding) : RecyclerView.ViewHolder(binding.root) {
+        private val tvTryCount = binding.tvTryCount
+        private val tvBall = binding.tvBall
+        private val tvStrike = binding.tvStrike
+        private val tvAnswer = binding.tvAnswer
+
+        fun bind(gameScore : Array<Int>) {
+            tvTryCount.text = "${gameScore[0]} 번째 도전"
+            tvBall.text = "볼 : ${gameScore[1]}"
+            tvStrike.text = "스트라이크 : ${gameScore[2]}"
+            tvAnswer.text = "입력 : ${gameScore[3]}"
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameResultViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.result_items, parent, false)
-        return GameResultViewHolder(view)
+        val viewBinding = ResultItemsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GameResultViewHolder(viewBinding)
     }
 
     override fun getItemCount(): Int = dataSet.size
@@ -34,7 +33,7 @@ class RecyclerViewAdapter() : RecyclerView.Adapter<RecyclerViewAdapter.GameResul
         holder.bind(dataSet[position])
     }
 
-    fun addData(gameScore: Array<String>) {
+    fun addData(gameScore: Array<Int>) {
         dataSet.add(0, gameScore)
         notifyItemInserted(0)
     }
